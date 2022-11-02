84be85f3b498797fbf2a74f135373a03e8b58407
Lucy Gelderloos <lucygel@gmail.com>
2022-11-01 20:53:28 -0700

    Create Main component with difficulty form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,40 @@
 import React from "react";
+import Form from 'react-bootstrap/Form';
+
+import TileBoard from './TileBoard.js';
+import ScoreBoard from './ScoreBoard.js';
 
 class Main extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+// Main contains (1) the form to select difficulty (2) the game board (3) the scoreboard
+
+    constructor(props) {
+        super(props);
+        this.state = {difficulty: 2};
+    }
+
+    handleSelect(event) {
+        this.setState({difficulty: event.target.value});
+        console.log(this.state.difficulty);
+    }    
 
     render() {
         return(
-            <div className="Main">
+            <main>
                 <h2>Main</h2>
-            </div>
+                <Form>
+                <Form.Group>
+                  <Form.Label>Choose difficulty level:</Form.Label>
+                  <Form.Select onSubmit={this.handleSelect.bind(this)}>      
+                    <option value="4">Easy</option>
+                    <option value="5">Medium</option>
+                    <option value="6">Hard</option>
+                    </Form.Select>
+                </Form.Group>
+                <input type="submit" value="Submit" />
+              </Form>
+              <TileBoard difficulty={this.props.difficulty} />
+              <ScoreBoard />
+            </main>
         )
     }
   }
