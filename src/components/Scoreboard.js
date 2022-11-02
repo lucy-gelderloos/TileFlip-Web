c131c340a26f0383c5b24d43443be96923da8beb
Lucy Gelderloos <lucygel@gmail.com>
2022-11-01 20:53:41 -0700

    Create ScoreBoard component

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ScoreBoard extends React.Component {
+    render() {
+        return(
+            <div className="scoreBoard">
+                <h2>ScoreBoard goes here</h2>
+            </div>
+        )
+    }
+}
+
+export default ScoreBoard;
